e91cdbe2c2c50045a18d9c92d62ebf7003bc19ee
Michael Körner <guiguigui@gmx.de>
2016-11-28 18:28:31 +0100

    #edit User and abilities for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  load_and_authorize_resource
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user.assign_attributes user_params
+    if @user.save
+      name = @user == current_user ? I18n.t(:your_account, scope: [:views, :common]) : "#{I18n.t(:account_of, scope: [:views, :common])} #{@user.name}"
+      redirect_to @user, notice: success_message(name: name)
+    else
+      render 'new'
+    end
+  end
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to root_path, :alert => "Access denied."
-    end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,9 @@ class Ability
   include CanCanNamespace::Ability
 
   def initialize(user)
-    can [:index, :show, :new], Game
-    can [:create, :edit, :update, :destroy], Game, user: user
+    can [:read, :create], Game
+    can [:update, :destroy], Game, user: user
+    can [:read], User, id: user.id
+    can [:update, :destroy], User, id: user.id
   end
 end
